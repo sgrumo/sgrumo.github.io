bcd505e52e13691c9b6c8c8a3d2b376a49206a03
Samuele Medici <samuele.medici@quinck.io>
2024-09-23 22:56:51 +0200

    build: :wrench: update astro with base repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://sgrumo.github.io',
-    base: 'personal-website',
+    base: 'sgrumo.github.io',
 });
 
